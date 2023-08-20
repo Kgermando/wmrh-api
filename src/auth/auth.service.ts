a4b62f8f6fcd677971467213a704f69829cb0115
Kgermando <katakugermain@gmail.com>
2023-08-20 01:47:31 +0100

    ['jwt']

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
 
     constructor( 
         private jwtService: JwtService,
+        private readonly config: ConfigService
         ) { }
 
     async personnelId(request: Request): Promise<number> {
-        const cookie = request.cookies['jwt'];
+        const token = this.config.get<string>('token');
+
+        // const cookie = request.cookies['jwt'];
+        const cookie = request.cookies[token];
 
         console.log(cookie);
 
