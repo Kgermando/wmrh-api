7e68b9a88d67dcfe2cbfdb574e952baf3a316dc8
Kgermando <katakugermain@gmail.com>
2023-09-07 18:47:20 +0100

    xxx

--- src/app.module.ts
+++ src/app.module.ts
@@ -45,7 +45,7 @@ import { AbonnementClientModule } from './admin/abonnement_client/abonnement_cli
         ssl:  {
           // ca: fs.readFileSync(path.join(__dirname, "/ssl/DigiCertGlobalRootG2.crt.pem")),
           // ca: configService.get<string>('database.ca'),
-          rejectUnauthorized: true,
+          rejectUnauthorized: false,
           require: true,
         },
         // ssl: process.env.NODE_ENV === "production" ? {
